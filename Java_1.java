0eb06f24581a412431943a90373432b09352300c
willydekeyser <ma074172@skynet.be>
2024-10-19 17:30:54 +0200

    add speed test

--- Java_1.java
+++ Java_1.java
@@ -1,7 +1,10 @@
+import java.util.concurrent.TimeUnit;
+
 public class Java_1 {
+    @SuppressWarnings("unused")
     public static void main(String[] args) {
         
-        System.out.println("JAVA - primitive data types in Java");
+        System.out.println("\n\nJAVA - primitive data types in Java");
         
             // 1 byte / -128 to 127
         byte myByte = 12;
@@ -16,24 +19,61 @@ public class Java_1 {
         System.out.println("myInt = " + myInt);
         
             // 8 bytes / -9.223.372.036.854.775.808 to 9.223.372.036.854.775.807
-        long myLong = 12l;
+        long myLong = 12; // 12 or 12l or 12L
         System.out.println("myLong = " + myLong);
 
             // 4 bytes / 6 decimal digits
-        float myFloat = 12f;
+        float myFloat = 12f; // 12f or 12F
         System.out.println("myFloat = " + myFloat);
 
             // 8 bytes / 15 decimal digits 
-        double myDouble = 12d;
+        double myDouble = 12; // 12 or 12d or 12D
         System.out.println("myDouble = " + myDouble);
         
             // 1 bit / true or false
-        boolean myBoolean = true;
+        boolean myBoolean = true; // true or false
         System.out.println("myBoolean = " + myBoolean);
 
             // 2 bytes / single character
-        char myChar = 'f';
-        System.out.println("myChar = " + myChar);
+        char myChar = 65; // 65 or 'A'
+        System.out.println("myChar = " + myChar + " - The ASCII code: " + (int)myChar);
+
+        int myTestInt = 0;
+        Integer mytestInteger = 0;
+        int COUNTER = 2147483647;
+
+        long start = System.nanoTime();
+        for(int x = 0; x < COUNTER; x++) {
+            myTestInt += x;
+        }
+        long stop = System.nanoTime();
+        for(Integer x = 0; x < COUNTER; x++) {
+            mytestInteger += x;
+        }
+        long end = System.nanoTime();
+        long intTime = TimeUnit.NANOSECONDS.toMillis(stop - start);
+        long integerTime = TimeUnit.NANOSECONDS.toMillis(end - stop);
+        System.out.println("Time int: " + intTime + " ms");
+        System.out.println("Time Integer: " + integerTime + " ms");
+        System.out.println("\n");
+
+        long myTestlong = 0;
+        Long mytestLong = 0L;
+
+        start = System.nanoTime();
+        for(long x = 0L; x < COUNTER; x++) {
+            myTestlong += x;
+        }
+        stop = System.nanoTime();
+        for(Long x = 0L; x < COUNTER; x++) {
+            mytestLong += x;
+        }
+        end = System.nanoTime();
+        long longTime = TimeUnit.NANOSECONDS.toMillis(stop - start);
+        long LongTime = TimeUnit.NANOSECONDS.toMillis(end - stop);
+        System.out.println("Time long: " + longTime + " ms");
+        System.out.println("Time Long: " + LongTime + " ms");
+        System.out.println("\n\n");
 
     }
 }
--- Java_2.java
+++ Java_2.java
@@ -6,69 +6,69 @@ public class Java_2 {
             // 1 byte / -128 to 127
         Byte myByte = 12;
         System.out.println("myByte = " + myByte);
-        System.out.println(Byte.TYPE);
-        System.out.println(Byte.BYTES);
-        System.out.println(Byte.SIZE);
-        System.out.println(Byte.MAX_VALUE);
-        System.out.println(Byte.MIN_VALUE);
+        System.out.println("Type: " + Byte.TYPE);
+        System.out.println("Bytes: " + Byte.BYTES);
+        System.out.println("Size: " + Byte.SIZE);
+        System.out.println("Max value: " + Byte.MAX_VALUE);
+        System.out.println("Min value: " + Byte.MIN_VALUE);
         System.out.println("\n");
 
             // 2 bytes / -32.768 to 32.767
         Short myShort = 12;
         System.out.println("myShort = " + myShort);
-        System.out.println(Short.TYPE);
-        System.out.println(Short.BYTES);
-        System.out.println(Short.SIZE);
-        System.out.println(Short.MAX_VALUE);
-        System.out.println(Short.MIN_VALUE);
+        System.out.println("Type: " + Short.TYPE);
+        System.out.println("Bytes: " + Short.BYTES);
+        System.out.println("Size: " + Short.SIZE);
+        System.out.println("Max value: " + Short.MAX_VALUE);
+        System.out.println("Min value: " + Short.MIN_VALUE);
         System.out.println("\n");
 
             // 4 bytes / -2.147.183.648 to 2.147.483.647
         Integer myInt = 12;
         System.out.println("myInt = " + myInt);
-        System.out.println(Integer.TYPE);
-        System.out.println(Integer.BYTES);
-        System.out.println(Integer.SIZE);
-        System.out.println(Integer.MAX_VALUE);
-        System.out.println(Integer.MIN_VALUE);
+        System.out.println("Type: " + Integer.TYPE);
+        System.out.println("Bytes: " + Integer.BYTES);
+        System.out.println("Size: " + Integer.SIZE);
+        System.out.println("Max value: " + Integer.MAX_VALUE);
+        System.out.println("Min value: " + Integer.MIN_VALUE);
         System.out.println("\n");
         
             // 8 bytes / -9.223.372.036.854.775.808 to 9.223.372.036.854.775.807
         Long myLong = 12l;
         System.out.println("myLong = " + myLong);
-        System.out.println(Long.TYPE);
-        System.out.println(Long.BYTES);
-        System.out.println(Long.SIZE);
-        System.out.println(Long.MAX_VALUE);
-        System.out.println(Long.MIN_VALUE);
+        System.out.println("Type: " + Long.TYPE);
+        System.out.println("Bytes: " + Long.BYTES);
+        System.out.println("Size: " + Long.SIZE);
+        System.out.println("Max value: " + Long.MAX_VALUE);
+        System.out.println("Min value: " + Long.MIN_VALUE);
         System.out.println("\n");
 
             // 4 bytes / 6 decimal digits
         Float myFloat = 12f;
         System.out.println("myFloat = " + myFloat);
-        System.out.println(Float.TYPE);
-        System.out.println(Float.BYTES);
-        System.out.println(Float.SIZE);
-        System.out.println(Float.MAX_VALUE);
-        System.out.println(Float.MIN_VALUE);
+        System.out.println("Type: " + Float.TYPE);
+        System.out.println("Bytes: " + Float.BYTES);
+        System.out.println("Size: " + Float.SIZE);
+        System.out.println("Max value: " + Float.MAX_VALUE);
+        System.out.println("Min value: " + Float.MIN_VALUE);
         System.out.println("\n");
 
             // 8 bytes / 15 decimal digits 
         Double myDouble = 12d;
         System.out.println("myDouble = " + myDouble);
-        System.out.println(Double.TYPE);
-        System.out.println(Double.BYTES);
-        System.out.println(Double.SIZE);
-        System.out.println(Double.MAX_VALUE);
-        System.out.println(Double.MIN_VALUE);
+        System.out.println("Type: " + Double.TYPE);
+        System.out.println("Bytes: " + Double.BYTES);
+        System.out.println("Size: " + Double.SIZE);
+        System.out.println("Max value: " + Double.MAX_VALUE);
+        System.out.println("Min value: " + Double.MIN_VALUE);
         System.out.println("\n");
         
             // 1 bit / true or false
         Boolean myBoolean = true;
         System.out.println("myBoolean = " + myBoolean);
-        System.out.println(Boolean.TYPE);
-        System.out.println(Boolean.FALSE);
-        System.out.println(Boolean.TRUE);
+        System.out.println("Type: " + Boolean.TYPE);
+        System.out.println("False value: " + Boolean.FALSE);
+        System.out.println("True value: " + Boolean.TRUE);
         System.out.println("\n");
         
             // 2 bytes / single character
