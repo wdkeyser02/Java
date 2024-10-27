00310279072b572afe4f87f22ae22843660f380e
willydekeyser <ma074172@skynet.be>
2024-10-27 08:42:41 +0100

    Setup

---
+++ Optional_1.java
@@ -0,0 +1,13 @@
+import java.util.Optional;
+
+public class Optional_1 {
+    public static void main(String[] args) {
+        
+
+        Optional<String> myOptional; // A container object which may or may not contain a non-null value. 
+                                                    // If a value is present, isPresent() returns true. 
+                                                    // If no value is present, the object is considered empty and isPresent() returns false.
+
+
+    }
+}
