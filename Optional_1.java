8e7e0881171ca9383531409d908e7217cd3741af
willydekeyser <ma074172@skynet.be>
2024-10-27 09:49:23 +0100

    Update

--- Optional_1.java
+++ Optional_1.java
@@ -1,13 +1,31 @@
 import java.util.Optional;
+import java.util.Random;
 
 public class Optional_1 {
     public static void main(String[] args) {
         
+        System.out.println("\nJava Optional.\n");
 
-        Optional<String> myOptional; // A container object which may or may not contain a non-null value. 
-                                                    // If a value is present, isPresent() returns true. 
-                                                    // If no value is present, the object is considered empty and isPresent() returns false.
+        Optional<String> myOptional = myRandomBoolean();    // A container object which may or may not contain a non-null value. 
+                                                            // If a value is present, isPresent() returns true. 
+                                                            // If no value is present, the object is considered empty and isPresent() returns false.
 
+        
+        System.out.println(myOptional.orElse("No text!"));
+        
+        if (myOptional.isPresent()) {
+            System.out.println("Optional is present!");            
+        } else {
+            System.out.println("Optional is not present!");   
+        }
+        System.out.println("\n");
+    }
 
+    private static Optional<String> myRandomBoolean() {
+        Random random = new Random();
+        if (random.nextBoolean()) {
+            return Optional.ofNullable("My Java Tutorials!");
+        }
+        return Optional.empty();
     }
 }
