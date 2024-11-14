bfb131d55c60a8f87f939921be465ab9e998e1df
willydekeyser <ma074172@skynet.be>
2024-11-14 16:20:36 +0100

    Update

--- Java_1.java
+++ Java_1.java
@@ -1,15 +1,15 @@
 public class Java_1 {
     public static void main(String[] args) {
-        
+
         System.out.println("\nJAVA Lambda\n");
         Runnable myRunnable = new Runnable() {
 
             @Override
             public void run() {
                 System.out.println("I am a Runnable");
-                
+
             }
-            
+
         };
 
         runSomthing(myRunnable);
