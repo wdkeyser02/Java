da42d1f33b555a4cc4a8ae1398e6ce0269f45f49
willydekeyser <ma074172@skynet.be>
2024-11-15 14:32:59 +0100

    Update

--- Java_1.java
+++ Java_1.java
@@ -6,11 +6,13 @@ public class Java_1 {
         JavaLoop_1 javaLoop_2 = new JavaLoop_1(2);
         JavaLoop_1 javaLoop_3 = new JavaLoop_1(3);
         JavaLoop_1 javaLoop_4 = new JavaLoop_1(4);
+        JavaLoop_1 javaLoop_5 = new JavaLoop_1(5);
 
         javaLoop_1.loop();
         javaLoop_2.loop();
         javaLoop_3.loop();
         javaLoop_4.loop();
+        javaLoop_5.loop();
         
         System.out.println("\nJava Loop, continue with the rest of the code!\n");
     }
