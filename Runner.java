85afeff1b47d1cde19027b14146015d94e5d1985
willydekeyser <ma074172@skynet.be>
2024-11-17 23:29:46 +0100

    Update

---
+++ Runner.java
@@ -0,0 +1,17 @@
+public class Runner {
+    public static void main(String[] args) {
+        
+        System.out.println("\nMain is Started!\n");
+        TaskScheduler scheduler = new TaskScheduler(2);
+        scheduler.schedule(() -> System.out.println("Task 1 executed at specific time"), System.currentTimeMillis() + 5000);
+        scheduler.scheduleAtFixedInterval(() -> System.out.println("Task 2 executed at fixed interval"), 2000);
+
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        scheduler.shutdown();
+    }
+}
