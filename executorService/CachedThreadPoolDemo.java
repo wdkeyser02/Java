97d0566d73dcb6cda3d67e801033a453299d0e36
willydekeyser <ma074172@skynet.be>
2024-11-18 04:07:48 +0100

    Update

---
+++ executorService/CachedThreadPoolDemo.java
@@ -0,0 +1,34 @@
+package executorService;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class CachedThreadPoolDemo {
+    public static void main(String[] args) {
+        
+        System.out.println("\nMain is Started!\n");
+        try (ExecutorService service = Executors.newCachedThreadPool()) {
+            for (int i = 0; i < 10000; i++) {
+                service.execute(new TaskOne(i));
+            }
+        }
+    }
+}
+
+class TaskOne implements Runnable {
+    private final int taskId;
+
+    public TaskOne(int taskId) {
+        this.taskId = taskId;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Task : " + taskId + " being executed by " + Thread.currentThread().getName());
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
---
+++ executorService/ScheduledExecutorDemo.java
@@ -0,0 +1,29 @@
+package executorService;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class ScheduledExecutorDemo {
+    public static void main(String[] args) {
+
+        System.out.println("\nMain is Started!\n");
+        ScheduledExecutorService executorService = Executors.newScheduledThreadPool(1);
+        executorService.scheduleAtFixedRate(new ProbeTask(), 1000, 2000, TimeUnit.MILLISECONDS);
+
+        try {
+            if (!executorService.awaitTermination(5000, TimeUnit.MILLISECONDS)) {
+                executorService.shutdownNow();
+            }
+        } catch (InterruptedException e) {
+            executorService.shutdownNow();
+        }
+    }
+}
+
+class ProbeTask implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("Probing end point for updates...");
+    }
+}
